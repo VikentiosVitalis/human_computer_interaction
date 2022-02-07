e4eec0fe47c844f4aef0e7256d3dfa86c351ab88
Stefanos Tsolos <43885015+stefanostsolos@users.noreply.github.com>
2022-02-07 19:36:53 +0200

    Create navigationScreen.dart

---
+++ byFigma/navigationScreen.dart
@@ -0,0 +1,253 @@
+      // Figma Flutter Generator NavigatingscreenWidget - FRAME
+      Container(
+      width: 428,
+      height: 926,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: -460,
+        left: -786,
+        child: Container(
+      width: 2000,
+      height: 2000,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+        width: 2000,
+        height: 2000,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Mapsiclemap.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 732,
+        left: 1046,
+        child: null
+      ),Positioned(
+        top: 956,
+        left: 1268,
+        child: null
+      ),Positioned(
+        top: 1936,
+        left: 1597,
+        child: null
+      ),Positioned(
+        top: 53,
+        left: 905,
+        child: null
+      ),Positioned(
+        top: 641.4113159179688,
+        left: 601.5376586914062,
+        child: null
+      ),Positioned(
+        top: 1131.790771484375,
+        left: 1112.3763427734375,
+        child: null
+      ),Positioned(
+        top: 1215.1552734375,
+        left: 1295.8870849609375,
+        child: null
+      ),Positioned(
+        top: 1369,
+        left: 410,
+        child: null
+      ),Positioned(
+        top: 888,
+        left: 532,
+        child: null
+      ),Positioned(
+        top: 1239,
+        left: 584,
+        child: null
+      ),Positioned(
+        top: 359.4430847167969,
+        left: 1018.0322265625,
+        child: null
+      ),Positioned(
+        top: 743,
+        left: 1691,
+        child: null
+      ),Positioned(
+        top: 1652,
+        left: 1479,
+        child: null
+      ),Positioned(
+        top: 347.18359375,
+        left: 379.48388671875,
+        child: null
+      ),Positioned(
+        top: 384.77935791015625,
+        left: 1237.209716796875,
+        child: null
+      ),Positioned(
+        top: 719,
+        left: 181,
+        child: null
+      ),Positioned(
+        top: 1846,
+        left: 309,
+        child: null
+      ),Positioned(
+        top: 1691,
+        left: 257,
+        child: null
+      ),Positioned(
+        top: 1800,
+        left: 716,
+        child: null
+      ),Positioned(
+        top: 988,
+        left: 785,
+        child: null
+      ),Positioned(
+        top: 1129,
+        left: 848,
+        child: null
+      ),Positioned(
+        top: 1131,
+        left: 976,
+        child: null
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 68,
+        left: 357,
+        child: Container(
+      width: 43,
+      height: 43,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 846,
+        left: 65,
+        child: Container(
+      width: 300,
+      height: 80,
+      decoration: BoxDecoration(
+          boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 702,
+        left: 333,
+        child: null
+      ),Positioned(
+        top: 139,
+        left: 270,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('EXIT NAVIGATION', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Roboto',
+        fontSize: 12,
+        letterSpacing: 1.25,
+        fontWeight: FontWeight.normal,
+        height: 1.3333333333333333
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 139,
+        left: 37,
+        child: Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('REACHED LOCATION?', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Roboto',
+        fontSize: 12,
+        letterSpacing: 1.25,
+        fontWeight: FontWeight.normal,
+        height: 1.3333333333333333
+      ),),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 66,
+        left: 26,
+        child: null
+      ),
+        ]
+      )
+    )
