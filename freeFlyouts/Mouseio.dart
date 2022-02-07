28749098350e2b117394904282d97f68e21e1700
Stefanos Tsolos <43885015+stefanostsolos@users.noreply.github.com>
2022-02-07 20:08:32 +0200

    Create Mouseio.dart

---
+++ freeFlyouts/Mouseio.dart
@@ -0,0 +1,206 @@
+      // Figma Flutter Generator MouseioflyoutWidget - INSTANCE
+      Container(
+      width: 344,
+      height: 340,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.20000000298023224),
+          offset: Offset(0,1),
+          blurRadius: 3
+      )],
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          null, SizedBox(width : 16),
+Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[Text('Mouseio', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 0.8700000047683716),
+        fontFamily: 'Roboto',
+        fontSize: 20,
+        letterSpacing: 0.15000000596046448,
+        fontWeight: FontWeight.normal,
+        height: 1.2
+      ),), SizedBox(height : 2),
+Text('-€/hour', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Roboto',
+        fontSize: 12,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1.75
+      ),),
+],
+      ),
+    ),
+
+        ],
+      ),
+    ), SizedBox(width : 0),
+null,
+
+        ],
+      ),
+    ), SizedBox(height : 0),
+null, SizedBox(height : 0),
+Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('1 km, Available', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 0.6000000238418579),
+        fontFamily: 'Roboto',
+        fontSize: 14,
+        letterSpacing: 0.25,
+        fontWeight: FontWeight.normal,
+        height: 1.4285714285714286
+      ),),
+
+        ],
+      ),
+    ), SizedBox(height : 0),
+Container(
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Button', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(98, 0, 238, 1),
+        fontFamily: 'Roboto',
+        fontSize: 14,
+        letterSpacing: 1.25,
+        fontWeight: FontWeight.normal,
+        height: 1.1428571428571428
+      ),),
+
+        ],
+      ),
+    ), SizedBox(width : 0),
+Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('GET DIRECTIONS', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Roboto',
+        fontSize: 14,
+        letterSpacing: 1.25,
+        fontWeight: FontWeight.normal,
+        height: 1.1428571428571428
+      ),),
+
+        ],
+      ),
+    ), SizedBox(width : 0),
+Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          null, SizedBox(width : 24),
+null,
+
+        ],
+      ),
+    ),
+
+        ],
+      ),
+    ),
+],
+      ),
+    ),
+],
+      ),
+    )
+      ),
+        ]
+      )
+    )
