a31c00055baa1246f10b10dc638648f4aaf4c423
Stefanos Tsolos <43885015+stefanostsolos@users.noreply.github.com>
2022-02-07 19:35:37 +0200

    Create mainMap.dart

---
+++ byFigma/mainMap.dart
@@ -0,0 +1,36 @@
+      // Figma Flutter Generator MapWidget - GROUP
+      Container(
+      width: 2000,
+      height: 2000,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+        width: 2000,
+        height: 2000,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Mapsiclemap.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 1134,
+        left: 979,
+        child: null
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    )
