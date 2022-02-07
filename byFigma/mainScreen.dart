9c219e04f9847211239d052fc9047c180f691ff6
Stefanos Tsolos <43885015+stefanostsolos@users.noreply.github.com>
2022-02-07 19:34:12 +0200

    Create mainScreen.dart

---
+++ byFigma/mainScreen.dart
@@ -0,0 +1,167 @@
+      // Figma Flutter Generator MainscreenWidget - FRAME
+      Container(
+      width: 428,
+      height: 926,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: -537,
+        left: -786,
+        child: Container(
+      width: 2000,
+      height: 2000,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+        width: 2000,
+        height: 2000,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Mapsiclemap.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 1134,
+        left: 979,
+        child: null
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 707,
+        left: 321,
+        child: null
+      ),Positioned(
+        top: 862,
+        left: 0,
+        child: Container(
+      decoration: BoxDecoration(
+          boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(0),
+            bottomLeft: Radius.circular(8),
+            bottomRight: Radius.circular(0),
+          ),
+      boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Free Parking', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Roboto',
+        fontSize: 24,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),),
+
+        ],
+      ),
+    ), SizedBox(width : 0),
+Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(0),
+            topRight: Radius.circular(8),
+            bottomLeft: Radius.circular(0),
+            bottomRight: Radius.circular(8),
+          ),
+      boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Private Parking', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Roboto',
+        fontSize: 24,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),),
+
+        ],
+      ),
+    ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 71,
+        left: 359,
+        child: Container(
+      width: 43,
+      height: 43,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 74,
+        left: 27,
+        child: null
+      ),
+        ]
+      )
+    )
