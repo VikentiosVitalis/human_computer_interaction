63cf6e0901193f2aaded7c5d3517889f8f270b38
Stefanos Tsolos <43885015+stefanostsolos@users.noreply.github.com>
2022-02-07 19:53:41 +0200

    Create shareAndWin.dart

---
+++ byFigma/shareAndWin.dart
@@ -0,0 +1,96 @@
+      // Figma Flutter Generator ShareandwinWidget - INSTANCE
+      Container(
+      width: 280,
+      height: 237,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 16,
+        left: 34,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[Text('Share and win 1€ discount!', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 0.8700000047683716),
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        letterSpacing: 0.15000000596046448,
+        fontWeight: FontWeight.normal,
+        height: 1.5
+      ),), SizedBox(height : 15),
+Text('Click below to copy a link directing to EasyParking app store page, then share with your friends. Win 1€ discount on your next parking order. ', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1.125
+      ),), SizedBox(height : 15),
+null,
+],
+      ),
+    )
+      ),Positioned(
+        top: 175,
+        left: 132,
+        child: Text('EasyParking.io/app', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1.125
+      ),)
+      ),Positioned(
+        top: 193,
+        left: 24,
+        child: Container(
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+      color : Color.fromRGBO(0, 0, 0, 1),
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Text('Done, back to map!', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'Roboto',
+        fontSize: 14,
+        letterSpacing: 1.25,
+        fontWeight: FontWeight.normal,
+        height: 1.1428571428571428
+      ),),
+
+        ],
+      ),
+    ),
+
+        ],
+      ),
+    )
+      ),
+        ]
+      )
+    )
